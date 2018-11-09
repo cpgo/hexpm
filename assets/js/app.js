a2f87a0b62fccb9d1559f7c3d362e72be0ebfd9e
Cassio Godinho <cassio.godinho@zup.com.br>
2018-11-09 18:16:30 -0200

    start webpack config

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,6 +13,8 @@
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
 
+import css from '../css/app.scss';
+window.$ = window.jQuery = require("../vendor/js/jquery");
 // Import local files
 //
 // Local files can be imported directly using relative
